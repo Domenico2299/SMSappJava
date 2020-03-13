9c189a50d4fa29af1833f2c544ec804f0598e7f4
Domenico <micuzzo22@Gmail.com>
2020-03-13 17:19:44 +0100

    Commit nuovo 2

--- app/src/main/java/file/smsappjava/MainActivity.java
+++ app/src/main/java/file/smsappjava/MainActivity.java
@@ -2,7 +2,11 @@ package file.smsappjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View view) {
+        EditText message_edittext = (EditText) findViewById(R.id.editText);
+        String message = message_edittext.getText().toString();
+        Uri uri = Uri.parse("smsto:0800000123");
+        Intent it = new Intent(Intent.ACTION_SENDTO, uri);
+        it.putExtra("sms_body", message);
+        startActivity(it);
     }
 }
